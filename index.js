e1bc6dce3795841f8a0f0d6697b42b0a9457d506
xavios <mr.acs.andras@gmail.com>
2017-03-14 10:21:49 +0100

    Hello world web app?!

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello-world', function (req, res) {
+   res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ "hello": "world" }, null, 3));
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
